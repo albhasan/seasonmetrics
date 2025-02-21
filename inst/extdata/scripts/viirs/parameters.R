1885d2bb21af55f3dbe9158f8931338981964da2
albhasan <albhasan@gmail.com>
2025-02-21 09:33:23 -0300

    Parameters updated.

--- inst/extdata/scripts/viirs/parameters.R
+++ inst/extdata/scripts/viirs/parameters.R
@@ -1,3 +1,8 @@
+# Input fire data.
+csv_files <- "/home/alber/documents/data/r_packages/seasonmetrics/viirs"
+stopifnot("Input directory not found!" = dir.exists(csv_files))
+
+# Directory for storing results.
 out_dir <-
   "/home/alber/Documents/results/r_packages/seasonmetrics/results_07/viirs"
 stopifnot("Output directory not found!" = dir.exists(out_dir))
