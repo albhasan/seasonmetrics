051e2e7e979f17ef8ee85e81c277bec49979635f
albhasan <albhasan@gmail.com>
2026-07-25 20:06:19 -0300

    Log additional parameters.

--- inst/extdata/scripts/modis/parameters_computing.R
+++ inst/extdata/scripts/modis/parameters_computing.R
@@ -1,5 +1,7 @@
 # Number of cores to use in parallel when processing CSV files.
 cores_process_csv <- 8L
+rlog::log_info("Number of cores for processing CSVs: ", cores_process_csv)
 
 # Number of cores to use in parallel to run regressions.
 cores_compute_season <- 16
+rlog::log_info("Number of cores for computing seasons: ", cores_process_season)
--- inst/extdata/scripts/modis/parameters_grid.R
+++ inst/extdata/scripts/modis/parameters_grid.R
@@ -1,5 +1,6 @@
 # Grid parameters.
-xy_min <- c(-180, -90)
-xy_max <- c(180, 90)
+xy_min <- c(xmin = -180, ymin = -90)
+xy_max <- c(xmax = 180, ymax = 90)
 grid_cells <- c(cols = 1440, rows = 720)
 grid_crs <- 4326
+rlog::log_info("Grid parameters: ", xy_min, xy_max, grid_cells, grid_crs)
